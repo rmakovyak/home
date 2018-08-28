cdfeddcfd5c5dbb8d28aafec9a7288328e7e33e8
Roman Makovyak <romanmakovyak@MacBook-Pro.local>
2018-08-28 21:31:42 +0200

    first working flow

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,15 +4,30 @@ import ProgressBar from './ProgressBar';
 import styles from './RegistrationForm.css';
 
 class RegistrationForm extends Component {
-  constructor(props) {
-    super(props);
+  static salaries = [
+    '0 - 1000',
+    '1000 - 2000',
+    '2000 - 3000',
+    '3000 - 4000',
+    'more than 4000'
+  ];
 
-    this.state = {
-      step: 0
-    };
+  state = {
+    step: 0,
+    name: '',
+    email: '',
+    phone: '',
+    salary: ''
+  };
+
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
-  nextStep() {
+  nextStep(e) {
+    e.preventDefault();
     this.setState({
       step: this.state.step + 1
     });
@@ -25,71 +40,78 @@ class RegistrationForm extends Component {
         {this.state.step === 0 && (
           <div className="RegistrationForm__group">
             <label>Name</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={e => this.handleInputChange(e)}
+            />
           </div>
         )}
         {this.state.step === 1 && (
           <div className="RegistrationForm__group">
             <label>Email</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={e => this.handleInputChange(e)}
+            />
           </div>
         )}
         {this.state.step === 2 && (
           <div className="RegistrationForm__group">
             <label>Phone number</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={e => this.handleInputChange(e)}
+            />
           </div>
         )}
         {this.state.step === 3 && (
           <div className="RegistrationForm__group">
             <label>Salary</label>
             <div className="RegistrationForm__radio-container">
-              <label className="RegistrationForm__radio">
-                <input type="radio" name="salary" value="1" />
-                0 - 1000
-              </label>
-              <label className="RegistrationForm__radio">
-                <input type="radio" name="salary" value="2" />
-                1000 - 2000
-              </label>
-              <label className="RegistrationForm__radio">
-                <input type="radio" name="salary" value="3" />
-                2000 - 3000
-              </label>
-              <label className="RegistrationForm__radio">
-                <input type="radio" name="salary" value="4" />
-                3000 - 4000
-              </label>
-              <label className="RegistrationForm__radio">
-                <input type="radio" name="salary" value="5" />
-                more than 4000
-              </label>
+              {RegistrationForm.salaries.map((el, index) => (
+                <label className="RegistrationForm__radio" key={index}>
+                  <input
+                    type="radio"
+                    name="salary"
+                    value={index}
+                    onChange={e => this.handleInputChange(e)}
+                  />
+                  {el}
+                </label>
+              ))}
             </div>
           </div>
         )}
         {this.state.step === 4 && (
           <div>
+            <p>You have successfully registered!</p>
             <div className="RegistrationForm__group">
               <label>Full name</label>
-              <span>Roman Makovyak</span>
+              <span>{this.state.name}</span>
             </div>
             <div className="RegistrationForm__group">
               <label>Email</label>
-              <span>romanmakovyak@gmail.com</span>
+              <span>{this.state.email}</span>
             </div>
             <div className="RegistrationForm__group">
               <label>Phone</label>
-              <span>romanmakovyak@gmail.com</span>
+              <span>{this.state.phone}</span>
             </div>
             <div className="RegistrationForm__group">
               <label>Salary</label>
-              <span>60000</span>
+              <span>{RegistrationForm.salaries[this.state.salary]}</span>
             </div>
           </div>
         )}
         {this.state.step < 4 && (
           <div className="RegistrationForm__group RegistrationForm__group--align-right">
-            <button type="button" onClick={() => this.nextStep()}>
+            <button type="submit" onClick={e => this.nextStep(e)}>
               Next
             </button>
           </div>
